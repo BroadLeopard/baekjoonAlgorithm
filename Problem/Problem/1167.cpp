fde4a1479a643321b986c0ddebaeed056312d6b7
BroadLeopard <whtngur155@gmail.com>
2020-03-15 02:21:56 +0900

    don't know what is wrong

--- Problem/Problem/1167.cpp
+++ Problem/Problem/1167.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int main()
 {
+	cin.tie(0);
+	cin.sync_with_stdio(false);
+
 	int n, x, y = 1, bigNode = 1, leng = 0, bigNode2 = 1, leng2 = 0;
 
 	cin >> n;
@@ -60,11 +63,8 @@ int main()
 	delete[] chk;
 	delete[] dis;
 
-
-	cout << bigNode;//가장 먼 점
-
-	bool* chk = new bool[n + 1]();
-	int* dis = new int[n + 1]();
+	bool* chk2 = new bool[n + 1]();
+	int* dis2 = new int[n + 1]();
 
 	que.push(bigNode);
 
@@ -74,24 +74,25 @@ int main()
 
 		for (int j = 0; j < arr[node].size(); j += 2)
 		{
-			if (chk[arr[node][j]] == false)
+			if (chk2[arr[node][j]] == false)
 			{
 				que.push(arr[node][j]);
 
-				dis[arr[node][j]] = dis[node] + arr[node][j + 1];
+				dis2[arr[node][j]] = dis2[node] + arr[node][j + 1];
 
-				if (dis[arr[node][j]] > leng2)
+				if (dis2[arr[node][j]] > leng2)
 				{
-					bigNode2 = arr[node][j];
+					leng2 = dis2[arr[node][j]];
 				}
 			}
 		}
-		chk[node] = true;
+		chk2[node] = true;
 		que.pop();
 	}
 
 	cout << leng2;
 
-
+	delete[] chk2;
+	delete[] dis2;
 	return 0;
 }
